02b3ebd538ec71426776d18fa70f67fab87e4243
VDucDung <dunga3qv2@gmail.com>
2024-10-07 11:06:54 +0700

    feat: api health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { All, Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Response } from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @All('/health-check')
+  healthCheck(@Res() res: Response): void {
+    res.send({
+      code: HttpStatus.OK,
+      message: HttpStatus[HttpStatus.OK],
+    });
   }
 }
