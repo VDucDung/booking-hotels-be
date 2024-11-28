16e7cbd3d9ea0532fce07cd82581f9e2a4354f43
VDucDung <dunga3qv2@gmail.com>
2024-11-28 23:26:09 +0700

    fix: update createBookingPaymentIntent

--- src/modules/stripe/stripe.service.ts
+++ src/modules/stripe/stripe.service.ts
@@ -68,18 +68,18 @@ export class StripeService {
         transfer_group: `booking_${stripeAccountId}`,
       });
 
-      const transfer = await this.stripe.transfers.create({
-        amount, //Math.floor(amount * 0.9)
-        currency,
-        destination: stripeAccountId,
-        transfer_group: `booking_${stripeAccountId}`,
-      });
+      // const transfer = await this.stripe.transfers.create({
+      //   amount, //Math.floor(amount * 0.9)
+      //   currency,
+      //   destination: stripeAccountId,
+      //   transfer_group: `booking_${stripeAccountId}`,
+      // });
 
       await this.ticketService.update(ticketId, user, {
         amount,
         paymentMethods: paymentMethod,
         stripePaymentIntentId: paymentIntent.id,
-        stripeTransferId: transfer.id,
+        // stripeTransferId: transfer.id,
       });
 
       this.logger.log(`Payment intent created for ticket ${ticketId}`);
@@ -465,4 +465,40 @@ export class StripeService {
       throw new Error('Failed to create payment session');
     }
   }
+
+  // async createPaymentIntent({
+  //   userId,
+  //   amount,
+  // }: {
+  //   userId: number;
+  //   amount: number;
+  // }): Promise<Stripe.Checkout.Session> {
+  //   if (!userId) {
+  //     throw new Error('User ID is required');
+  //   }
+
+  //   if (!amount || amount < 12000) {
+  //     throw new Error('Invalid amount. Minimum deposit is ₫12,000.');
+  //   }
+
+  //   try {
+  //     const paymentIntent = await this.stripe.paymentIntents.create({
+  //       amount: amount * 100,
+  //       currency: 'vnd',
+  //       metadata: { bookingId },
+  //     });
+
+  //     await this.transactionService.createTransaction({
+  //       userId,
+  //       amount,
+  //       type: TransactionType.DEPOSIT,
+  //       stripeSessionId: session.id,
+  //     });
+
+  //     return paymentIntent.client_secret;
+  //   } catch (error) {
+  //     this.logger.error('Error creating checkout session', error);
+  //     throw new Error('Failed to create payment session');
+  //   }
+  // }
 }
